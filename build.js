b38bb4707efcc4eb028aeeb620494c4e6932f0fb
Jesse Jurman <j.r.jurman@gmail.com>
2023-10-08 10:27:12 -0400

    more comments for build.js

--- build.js
+++ build.js
@@ -1,13 +1,20 @@
+// This script contains all the steps for generating the output results.
+// This can be triggered by running `npm run build`
+//
+// A majority of this file uses the UglifyJS API
+// https://www.npmjs.com/package/uglify-js
+//
 const fs = require('fs');
 const path = require('path');
 const UglifyJS = require('uglify-js');
 const { version } = require('./package.json');
 
-// ensure the output directory exists
+// before we start anything, make sure the output directory exists and is empty
 if (!fs.existsSync('output')) {
+	// if it doesn't exist make it
 	fs.mkdirSync('output');
 } else {
-	// If it exists, read all the files inside the directory and remove them
+	// if it does, remove all files in the directory
 	const files = fs.readdirSync('output');
 	for (const file of files) {
 		const filePath = path.join('output', file);
@@ -15,7 +22,7 @@ if (!fs.existsSync('output')) {
 	}
 }
 
-// load all files
+// load all source class files (these will be included in all builds)
 const classFiles = ['src/TramLite.js', ...fs.readdirSync('src/processors').map((file) => `src/processors/${file}`)];
 const loadedClassFiles = Object.fromEntries(
 	classFiles.map((filePath) => {
@@ -24,6 +31,7 @@ const loadedClassFiles = Object.fromEntries(
 	}),
 );
 
+// load all import/export scripts separately (these are only included in some builds)
 console.log('loading', 'src/import-components.js');
 const importComponentClass = {
 	'src/ImportComponent.js': fs.readFileSync('src/ImportComponent.js').toString(),
@@ -33,8 +41,12 @@ const importScript = {
 	'src/scripts/import-script.js': fs.readFileSync('src/scripts/import-script.js').toString(),
 };
 
-// Set configurations
-const setConfigs = [
+// uglify parameters to change the result of each bundle.
+// `MODULE` and `INSTALL` are variables that can be found in the class files and
+//   determine if we should attach listeners for a window or export the class for a JS API.
+// `enclose` determines if the code should be wrapped in an IIFE (which prevents
+//   prevents class definitions from colliding).
+const buildConfigs = [
 	{
 		outputFile: 'output/api.js',
 		files: loadedClassFiles,
@@ -58,7 +70,7 @@ const setConfigs = [
 	},
 ];
 
-setConfigs.forEach((config) => {
+buildConfigs.forEach((config) => {
 	console.log('building', config.outputFile);
 	const options = {
 		compress: {
@@ -77,7 +89,7 @@ setConfigs.forEach((config) => {
 	fs.writeFileSync(config.outputFile, result.code);
 });
 
-// Minify
+// for each of these, create a minified version
 const minifyConfigs = [
 	{ inputFile: 'output/api.js', outputFile: 'output/api.min.js' },
 	{ inputFile: 'output/tram-lite.js', outputFile: 'output/tram-lite.min.js' },
@@ -91,7 +103,7 @@ minifyConfigs.forEach((config) => {
 	fs.writeFileSync(config.outputFile, result.code);
 });
 
-// do a simple copy for the export-script
+// do a simple copy for the export-script (needs no minification)
 fs.copyFileSync('src/scripts/export-script.js', 'output/export-components.js');
 
-console.log('Build complete!');
+console.log('Tram-Lite build complete!');
